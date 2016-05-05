1ed18cae025eaa08160c2a7e7842dca8180fe527
marcin.wolicki <marcin.wolicki@systegra.pl>
2016-05-05 20:37:36 +0100

    map struct types

--- FSharpDump/Program.fs
+++ FSharpDump/Program.fs
@@ -6,67 +6,81 @@ module MiniWinDbg =
     open System
     open Microsoft.Diagnostics.Runtime
 
+    type Type = {
+        Name : string
+        Type : ClrType
+    }
+
     type Object = {
         Id : UInt64
-        Type : ClrType
-        Fields : Map<string, Val>
+        Type : Type
+        Fields : unit -> Map<string, Val>
         Size : UInt64 }
     and ValueType = {
-        Type : ClrType
+        Type : Type
         //Fields : Map<string, Val option>
         Size : UInt64 }
     and Val =
     | Obj of Object option
     | Boolean of bool
     | Char of char
-    | Double of double
-
+    | Float of float
+    | Float32 of float32
+    | Int8 of int8
+    | Int16 of int16
+    | Int32 of int32
+    | Int64 of int64
+    | UInt8 of uint8
+    | UInt16 of uint16
+    | UInt32 of uint32
+    | UInt64 of uint64
+    | NativeInt of nativeint
+    | UNativeInt of unativeint
     | Struct of ValueType
 
     type Runtime = {
-        Types : ClrType seq
-        Objects : Object seq
-    }
+        Types : Type seq
+        Objects : Object seq }
+
     [<AutoOpen>]
     module private MiniWinDbg =
 
         let getValueType t size =
             { Type = t
               Size = uint64 size }
-
+        let toType t = { Type = t; Name = t.Name }
         let rec getObject (heap:ClrHeap) addr =
             let t = heap.GetObjectType addr
             if t = null then None
             else
-//                let fields = t.Fields 
-//                             |> Seq.map (fun x->x.Name, match x.ElementType with 
-//                                                        | ClrElementType.Struct
-//                                                        | ClrElementType.Boolean
-//                                                        | ClrElementType.Char
-//                                                        | ClrElementType.Double
-//                                                        | ClrElementType.Float
-//                                                        | ClrElementType.Int16
-//                                                        | ClrElementType.Int32
-//                                                        | ClrElementType.Int64
-//                                                        | ClrElementType.Int8
-//                                                        | ClrElementType.UInt16
-//                                                        | ClrElementType.UInt32
-//                                                        | ClrElementType.UInt64
-//                                                        | ClrElementType.UInt8
-//                                                        | ClrElementType.NativeInt
-//                                                        | ClrElementType.NativeUInt
-//                                                        | ClrElementType.Pointer ->
-//                                                            getValueType x.Type x.Size |> ValType
-//                                                        | _ -> 
-//                                                            let addr = x.GetAddress(addr, false)
-//                                                            getObject heap addr |> Obj)
-//                             |> Map.ofSeq
-                
-            
+                let field = fun () -> t.Fields 
+                                      |> Seq.map (fun x->x.Name, match x.ElementType with 
 
-                { Type = t
+                                                                 | ClrElementType.Int64 -> x.GetValue addr :?> int64 |> Int64
+                                                                 | ClrElementType.Boolean -> x.GetValue addr :?> bool |> Boolean
+                                                                 | ClrElementType.Char -> x.GetValue addr :?> char |> Char
+                                                                 | ClrElementType.Double -> x.GetValue addr :?> float32 |> Float32
+                                                                 | ClrElementType.Float -> x.GetValue addr :?> float |> Float
+                                                                 | ClrElementType.Int16 -> x.GetValue addr :?> int16 |> Int16
+                                                                 | ClrElementType.Int32 -> x.GetValue addr :?> int32 |> Int32
+                                                                 | ClrElementType.Int8 -> x.GetValue addr :?> int8 |> Int8
+                                                                 | ClrElementType.UInt16 -> x.GetValue addr :?> uint16 |> UInt16
+                                                                 | ClrElementType.UInt32 -> x.GetValue addr :?> uint32 |> UInt32
+                                                                 | ClrElementType.UInt64 -> x.GetValue addr :?> uint64 |> UInt64
+                                                                 | ClrElementType.UInt8 -> x.GetValue addr :?> uint8 |> UInt8
+                                                                 | ClrElementType.NativeInt -> x.GetValue addr :?> nativeint |> NativeInt
+                                                                 | ClrElementType.NativeUInt -> x.GetValue addr :?> unativeint |> UNativeInt
+                                                                 | ClrElementType.Struct
+                                                                 | ClrElementType.Pointer ->
+                                                                     getValueType (toType x.Type) x.Size |> Struct
+                                                                 | _ -> 
+                                                                     let addr = x.GetAddress(addr, false)
+                                                                     getObject heap addr |> Obj)
+                                      |> Map.ofSeq
+
+                { Type = toType t
                   Size = t.GetSize addr
-                  Fields = Map<_,_>[]
+                  Fields = field
                   Id = addr } |> Some
             
 
@@ -81,7 +95,7 @@ module MiniWinDbg =
         let runtimes = target.ClrVersions |> Seq.map (fun x-> x.CreateRuntime(getDac x)) |> Seq.toArray
 
         let runtime ={
-            Types = runtimes |> Seq.collect (fun x->x.GetHeap().EnumerateTypes())
+            Types = runtimes |> Seq.collect (fun x->x.GetHeap().EnumerateTypes()) |> Seq.map (fun t-> { Name = t.Name; Type = t})
             Objects = runtimes |> getObjects }
 
 
@@ -89,17 +103,20 @@ module MiniWinDbg =
 
 
 
-let d, runtimes = MiniWinDbg.openDumpFile @"C:\memdumps\MHODumpIntegrated\SimCorp.IMS.OM.Client.Application.DMP"
-let zz =
-    runtimes.Types 
-        |> Array.ofSeq 
-        |> Array.collect(fun x->x.Methods |> Seq.filter(fun p->p.CompilationType = Microsoft.Diagnostics.Runtime.MethodCompilationType.None) |> Seq.toArray)
+let d, runtimes = MiniWinDbg.openDumpFile @"C:\tmp\example.dmp"
 
-let zzz = zz |> Array.filter (fun x ->x.GetFullSignature().Contains "SimCorp.IMS.")
+open System.Diagnostics
+
+let objects = runtimes.Objects |> Array.ofSeq
+
+objects |> Array.filter (fun x->x.Type.Name = "example.classA") |> Array.length = 10 |> Debug.Assert
+objects |> Array.filter (fun x->x.Type.Name = "example.classA[]") |> Array.length = 1 |> Debug.Assert
+
+let classA = objects |> Array.filter (fun x->x.Type.Name = "example.classA") |> Array.head
+classA.Fields().["Long"]
 
 
-let a = runtimes.Objects 
-        |> Array.ofSeq
 
-let group = a |> Array.groupBy (fun x->x.Type.Name) |> Array.Parallel.map(fun (a,b)->a, (b, b |> Array.sumBy (fun p->p.Size))) |> Array.sortByDescending (fun (_,(_,s)) ->s)
+let o = objects |> Array.filter (fun x->x.Type.Name.Contains "classA")
+
 
