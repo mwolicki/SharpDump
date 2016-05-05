43417953531f60e7c3d4318d5504d270222d18fb
marcin.wolicki <marcin.wolicki@systegra.pl>
2016-05-05 21:48:55 +0100

    support for nested objects

--- FSharpDump/Program.fs
+++ FSharpDump/Program.fs
@@ -18,7 +18,7 @@ module MiniWinDbg =
         Size : UInt64 }
     and ValueType = {
         Type : Type
-        //Fields : Map<string, Val option>
+        Fields : unit -> Map<string, Val>
         Size : UInt64 }
     and Val =
     | Obj of Object option
@@ -37,6 +37,7 @@ module MiniWinDbg =
     | NativeInt of nativeint
     | UNativeInt of unativeint
     | Struct of ValueType
+    | String of String
 
     type Runtime = {
         Types : Type seq
@@ -45,42 +46,47 @@ module MiniWinDbg =
     [<AutoOpen>]
     module private MiniWinDbg =
 
-        let getValueType t size =
-            { Type = t
+        let rec getFields (t:ClrType) (addr:uint64) = 
+            fun () -> t.Fields 
+                       |> Seq.map (fun x->x.Name, match x.ElementType with 
+
+                                                   | ClrElementType.Int64 -> x.GetValue addr :?> int64 |> Int64
+                                                   | ClrElementType.Boolean -> x.GetValue addr :?> bool |> Boolean
+                                                   | ClrElementType.Char -> x.GetValue addr :?> char |> Char
+                                                   | ClrElementType.Double -> x.GetValue addr :?> float32 |> Float32
+                                                   | ClrElementType.Float -> x.GetValue addr :?> float |> Float
+                                                   | ClrElementType.Int16 -> x.GetValue addr :?> int16 |> Int16
+                                                   | ClrElementType.Int32 -> x.GetValue addr :?> int32 |> Int32
+                                                   | ClrElementType.Int8 -> x.GetValue addr :?> int8 |> Int8
+                                                   | ClrElementType.UInt16 -> x.GetValue addr :?> uint16 |> UInt16
+                                                   | ClrElementType.UInt32 -> x.GetValue addr :?> uint32 |> UInt32
+                                                   | ClrElementType.UInt64 -> x.GetValue addr :?> uint64 |> UInt64
+                                                   | ClrElementType.UInt8 -> x.GetValue addr :?> uint8 |> UInt8
+                                                   | ClrElementType.NativeInt -> x.GetValue addr :?> nativeint |> NativeInt
+                                                   | ClrElementType.NativeUInt -> x.GetValue addr :?> unativeint |> UNativeInt
+                                                   | ClrElementType.Struct
+                                                   | ClrElementType.Pointer -> //TODO
+                                                       getValueType x.Type x.Size addr |> Struct
+                                                   | ClrElementType.String -> x.GetValue addr :?> string |> String
+                                                   | ClrElementType.SZArray //TODO
+                                                   | ClrElementType.Object ->
+                                                       let addr = x.GetAddress(addr, t.IsValueClass)
+                                                       printfn " addr = %A" addr
+                                                       getObject t.Heap addr |> Obj
+                                                   | t -> sprintf "Unsupported type %O" t |> failwith)
+                       |> Map.ofSeq
+        and getValueType t size addr =
+            { Type = toType t
+              Fields = getFields t addr
               Size = uint64 size }
-        let toType t = { Type = t; Name = t.Name }
-        let rec getObject (heap:ClrHeap) addr =
+        and toType t = { Type = t; Name = t.Name }
+        and getObject (heap:ClrHeap) addr =
             let t = heap.GetObjectType addr
             if t = null then None
             else
-                let field = fun () -> t.Fields 
-                                      |> Seq.map (fun x->x.Name, match x.ElementType with 
-
-                                                                 | ClrElementType.Int64 -> x.GetValue addr :?> int64 |> Int64
-                                                                 | ClrElementType.Boolean -> x.GetValue addr :?> bool |> Boolean
-                                                                 | ClrElementType.Char -> x.GetValue addr :?> char |> Char
-                                                                 | ClrElementType.Double -> x.GetValue addr :?> float32 |> Float32
-                                                                 | ClrElementType.Float -> x.GetValue addr :?> float |> Float
-                                                                 | ClrElementType.Int16 -> x.GetValue addr :?> int16 |> Int16
-                                                                 | ClrElementType.Int32 -> x.GetValue addr :?> int32 |> Int32
-                                                                 | ClrElementType.Int8 -> x.GetValue addr :?> int8 |> Int8
-                                                                 | ClrElementType.UInt16 -> x.GetValue addr :?> uint16 |> UInt16
-                                                                 | ClrElementType.UInt32 -> x.GetValue addr :?> uint32 |> UInt32
-                                                                 | ClrElementType.UInt64 -> x.GetValue addr :?> uint64 |> UInt64
-                                                                 | ClrElementType.UInt8 -> x.GetValue addr :?> uint8 |> UInt8
-                                                                 | ClrElementType.NativeInt -> x.GetValue addr :?> nativeint |> NativeInt
-                                                                 | ClrElementType.NativeUInt -> x.GetValue addr :?> unativeint |> UNativeInt
-                                                                 | ClrElementType.Struct
-                                                                 | ClrElementType.Pointer ->
-                                                                     getValueType (toType x.Type) x.Size |> Struct
-                                                                 | _ -> 
-                                                                     let addr = x.GetAddress(addr, false)
-                                                                     getObject heap addr |> Obj)
-                                      |> Map.ofSeq
-
                 { Type = toType t
                   Size = t.GetSize addr
-                  Fields = field
+                  Fields = getFields t addr
                   Id = addr } |> Some
             
 
@@ -113,10 +119,7 @@ objects |> Array.filter (fun x->x.Type.Name = "example.classA") |> Array.length 
 objects |> Array.filter (fun x->x.Type.Name = "example.classA[]") |> Array.length = 1 |> Debug.Assert
 
 let classA = objects |> Array.filter (fun x->x.Type.Name = "example.classA") |> Array.head
-classA.Fields().["Long"]
-
-
-
-let o = objects |> Array.filter (fun x->x.Type.Name.Contains "classA")
+let (MiniWinDbg.Struct p) = classA.Fields().["structA"]
+p.Fields().["c"]
 
 
